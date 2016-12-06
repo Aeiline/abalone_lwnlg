0a65bdaa1eeec63c77d53a201e3f821d1dda2365
wautel <lucie.wautelet@epitech.eu>
2016-12-06 12:57:05 +0000

    gamelogic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -7,16 +7,19 @@ class GameLogic {
 	
 	public void click_occured(Cell clickedcell)
 	{
-		//last_pos_x = clickedcell.
+		case_number = 1;
+		
+		last_pos = clickedcell.getBoardPos(); 
+
 		if (clickedcell.getPlayer() == -1)
 		{
-			case_number = 0;
 			print_case_free();
 			//affichage des cases disponibles
 		}
 		else if (clickedcell.getPlayer() == player_turn)
 		{
 			save_case();
+	//TODO nolwenn		clickedcell.setSelected();
 			//tour joue
 		}
 	}
@@ -26,16 +29,18 @@ class GameLogic {
 		case_number += 1;
 		if (case_to_move == null)
 		{
-			case_to_move = new int[2][3];
-			for (int i = 0; i < 2; i +=1)
+			case_to_move = new int[3][2];
+			for (int i = 0; i < 3; i +=1)
 			{
-				for(int j = 0; j < 3; j += 1)
+				for(int j = 0; j < 2; j += 1)
 				{
 					case_to_move[i][j] = -1;
 				}
 			}
 		}
 		System.out.println("nombre " + case_number);
+		case_to_move[case_number][0] = last_pos[0];
+		case_to_move[case_number][1] = last_pos[1];
 	}
 	
 	private void print_case_free()
@@ -47,8 +52,8 @@ class GameLogic {
 	private int case_number;
 	private int[][] case_to_move;
 	private int player_turn;
-	private int last_pos_x;
-	private int last_pos_y;
+	private int[] last_pos;
+
 	
 	
 	
